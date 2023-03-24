ac69fd0767329e456d177938acc353e200104337
betwensy <bunyawat.program@gmail.com>
2023-03-24 12:44:49 +0700

    work1 bunyawat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Admin1Component } from './admin1/admin1.component';
+import { Admin2Component } from './admin2/admin2.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { SigninComponent } from './signin/signin.component';
@@ -12,6 +14,8 @@ const routes: Routes = [
   { path: "register", component: RegisterComponent },
   { path: "", component: SigninComponent },
   { path: "splash", component: SplashComponent },
+  { path: "admin1", component: Admin1Component },
+  { path: "admin2", component: Admin2Component },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { RegisterComponent } from './register/register.component';
 import { SigninComponent } from './signin/signin.component';
 import { LoginComponent } from './login/login.component';
 import { SplashComponent } from './splash/splash.component';
-import { Admin1Component } from './admin1/admin1.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +18,7 @@ import { Admin1Component } from './admin1/admin1.component';
     SigninComponent,
     LoginComponent,
     SplashComponent,
-    Admin1Component
+
   ],
   imports: [
     BrowserModule,
