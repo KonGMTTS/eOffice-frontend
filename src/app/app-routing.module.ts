c17d4a6e4301cb0e8f844ce60889c76346bd2e10
KonGMTTS <kasidej23@gmail.com>
2023-03-27 10:55:32 +0700

    add_bunyawat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Admin1Component } from './admin1/admin1.component';
+import { Admin2Component } from './admin2/admin2.component';
+import { Admin3Component } from './admin3/admin3.component';
+import { Admin4Component } from './admin4/admin4.component';
+import { Admin7Component } from './admin7/admin7.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { SigninComponent } from './signin/signin.component';
@@ -13,6 +18,13 @@ const routes: Routes = [
   { path: "register", component: RegisterComponent },
   { path: "", component: SigninComponent },
   { path: "splash", component: SplashComponent },
+  { path: "user1splash", component: User1Component },
+  { path: "admin1", component: Admin1Component },
+  { path: "admin2", component: Admin2Component },
+  { path: "admin3", component: Admin3Component },
+  { path: "admin4", component: Admin4Component },
+  { path: "admin7", component: Admin7Component },
+
 ];
 
 @NgModule({
