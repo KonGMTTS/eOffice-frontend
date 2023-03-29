2e4bfe322ee9310648e1ea3c92081777699a1fcf
thanasitp <thanasitnavy60@gmail.com>
2023-03-29 15:19:52 +0700

    thanasit

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import axios from 'axios';
 
 @Component({
   selector: 'app-signin',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent {
+  public nameForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.nameForm = this.formBuilder.group({
+      email: '',
+      password: '',
+    });
+  }
 
+  signin() {
+    let data = {
+      "userEmail": this.nameForm.get('email')?.value,
+      "Password": this.nameForm.get('password')?.value
+    }
+    console.log(data);
+
+    axios.get("http://10.104.6.212:1337/api/rtarf-user-register-g1s?filters[userEmail][$eq]=" + this.nameForm.get('email')?.value).then(res => {
+      console.log(res.data);
+    })
+  }
 }
