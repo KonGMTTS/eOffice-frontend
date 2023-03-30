6eb135682a0a4832725539776b1eef55778d4c0c
sunan1919 <szaaa06@gmail.com>
2023-03-30 11:43:24 +0700

    UploadFile

--- src/app/admin1/admin1.component.ts
+++ src/app/admin1/admin1.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import axios from 'axios';
 @Component({
   selector: 'app-admin1',
   templateUrl: './admin1.component.html',
   styleUrls: ['./admin1.component.scss']
 })
 export class Admin1Component {
+  myForm = new FormGroup({
+    file: new FormControl('', [Validators.required]),
+    fileSource: new FormControl('', [Validators.required])
+  });
+
+  files: Array<any> = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  get f() {
+    return this.myForm.controls;
+  }
+
+  onFileChange(event: any) {
+
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.myForm.patchValue({
+        fileSource: file
+      });
+    }
+  }
+
+  uploadFile() {
+    const formData = new FormData();
+    formData.append('files', this.myForm.get('fileSource')?.value || '{}');
+
+    axios.post("http://10.104.6.212:1337/api/upload", formData).then((res: any) => {
+      console.log(res.data);
+      let file = res.data[0]
+      this.files.push(file.name)
+    }).catch(error => {
+      console.log(error.message);
+    });
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,12 @@ import { MenuComponent } from './menu/menu.component';
 import { User2Component } from './user2/user2.component';
 import { User5Component } from './user5/user5.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Admin1Component } from './admin1/admin1.component';
+import { Admin2Component } from './admin2/admin2.component';
+import { Admin3Component } from './admin3/admin3.component';
+import { Admin4Component } from './admin4/admin4.component';
+import { User3Component } from './user3/user3.component';
+import { User4Component } from './user4/user4.component';
 
 
 @NgModule({
@@ -25,6 +31,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     RegisterComponent,
     SigninComponent,
     LoginComponent,
+    Admin1Component,
+    Admin2Component,
+    Admin3Component,
+    Admin4Component,
     SplashComponent,
     Admin7Component,
     User1Component,
@@ -32,7 +42,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     Admin6Component,
     MenuComponent,
     User2Component,
-    User5Component
+    User3Component,
+    User4Component,
+    User5Component,
   ],
   imports: [
     BrowserModule,
